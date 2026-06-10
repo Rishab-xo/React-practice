449143f00c6bd91b252e5174ec0d5f33115fc203
Rishab <rishabx0@gmail.com>
2026-06-10 18:26:57 +0530

    Routing Project is done & dusted

---
+++ Learning React/10-RouterDom/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Routes,Route, Link } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+
+      <Navbar/>
+
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/About' element={<About/>}/>
+      <Route path='/Contact' element={<Contact/>}/>
+    </Routes>
+
+    </div>
+  )
+}
+
+export default App
---
+++ Learning React/10-RouterDom/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="nav">
+        <h3>Team Diamonds</h3>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/About">About</Link>
+          <Link to="/Contact">Contact</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
